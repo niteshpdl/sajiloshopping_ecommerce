5d6f28042b6c146c15db24b5c412c266d5276fdb
Nitesh Paudel <niteshpaudel2004@gmail.com>
2024-12-13 22:34:03 +0545

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database configuration
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///' + str(BASE_DIR / 'db.sqlite3')),
+        default=os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')),
         conn_max_age=600,
         conn_health_checks=True,
     )
