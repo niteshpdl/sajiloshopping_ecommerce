36738ba2fe46d3918907fb86681782e315c55cfc
Nitesh Paudel <niteshpaudel2004@gmail.com>
2024-12-14 09:41:58 +0545

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -128,7 +128,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # You can keep this as is for loca
 
 # Cloudinary settings for media files
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'dh5woyv9s',
+    'CLOUD_NAME': 'untitled',
     'API_KEY': '633598579816969',
     'API_SECRET': 'u49cpUP7kvbDN7yT2m2HDYMY-ss',
     'SECURE': True,
