04126854020dbf0db9ec02b5a5b099b89c570c2f
Nitesh Paudel <niteshpaudel2004@gmail.com>
2024-12-14 18:54:11 +0545

    Update models.py

--- store/models.py
+++ store/models.py
@@ -7,19 +7,20 @@ from cloudinary.models import CloudinaryField
 
 # Create Customer Profile
 class Profile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-        date_modified = models.DateTimeField(auto_now=True)
-	phone = models.CharField(max_length=20, blank=True)
-	address1 = models.CharField(max_length=200, blank=True)
-	address2 = models.CharField(max_length=200, blank=True)
-	city = models.CharField(max_length=200, blank=True)
-	state = models.CharField(max_length=200, blank=True)
-	zipcode = models.CharField(max_length=200, blank=True)
-	country = models.CharField(max_length=200, blank=True)
-	old_cart = models.CharField(max_length=200, blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(auto_now=True)
+    phone = models.CharField(max_length=20, blank=True)
+    address1 = models.CharField(max_length=200, blank=True)
+    address2 = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=200, blank=True)
+    state = models.CharField(max_length=200, blank=True)
+    zipcode = models.CharField(max_length=200, blank=True)
+    country = models.CharField(max_length=200, blank=True)
+    old_cart = models.CharField(max_length=200, blank=True, null=True)
 
-	def __str__(self):
-		return self.user.username
+    def __str__(self):
+        return self.user.username
+
 
 # Create a user Profile by default when user signs up
 def create_profile(sender, instance, created, **kwargs):
